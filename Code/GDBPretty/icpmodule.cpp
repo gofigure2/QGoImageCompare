6f3f466c614f1c853097e575e480a5579074dcac
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-08-19 15:56:47 -0500

    Start creation of the ICPPrinter python class.
    My first creation of a C Python class, so lots of comments of my understanding.
    
    Builds and runs fine, but warning on line 190 "dereferencing type-punned pointer
    will break strict-aliasing rules".  Not sure why that occurs, so I will not try
    to override it for now.

--- Code/GDBPretty/icpmodule.cpp
+++ Code/GDBPretty/icpmodule.cpp
@@ -2,6 +2,134 @@
 
 #include <iostream>
 
+/** Our gdb pretty printer class.  An instantiation of this struct is like a C++
+ * class this*, but in python it is traditionally call self*.
+ */
+typedef struct
+{
+  PyObject_HEAD
+} ICPPrinter;
+
+/** This is equivalent in C++ to the constructor for ICPPrinter. */
+static PyObject *
+ICPPrinter_new( PyTypeObject *type, PyObject *args, PyObject *kwds )
+{
+  ICPPrinter *self;
+
+  self = (ICPPrinter *)type->tp_alloc(type, 0);
+  if ( self != NULL )
+  {
+      //self->first = PyString_FromString("");
+      //if (self->first == NULL)
+        //{
+          //Py_DECREF(self);
+          //return NULL;
+        //}
+
+      //self->last = PyString_FromString("");
+      //if (self->last == NULL)
+        //{
+          //Py_DECREF(self);
+          //return NULL;
+        //}
+
+      //self->number = 0;
+  }
+
+  return (PyObject *)self;
+}
+
+/** This is equivalent in C++ to the destructor for ICPPrinter. */
+static void
+ICPPrinter_dealloc( ICPPrinter * self )
+{
+  self->ob_type->tp_free((PyObject*)self);
+}
+
+
+/** This defines the python constructor that would normally be defined with
+ * __init__(self) in python. */
+static int
+ICPPrinter_init( ICPPrinter * self, PyObject * args, PyObject * kwds )
+{
+    //PyObject *first=NULL, *last=NULL, *tmp;
+
+    //static char *kwlist[] = {"first", "last", "number", NULL};
+
+    //if (! PyArg_ParseTupleAndKeywords(args, kwds, "|OOi", kwlist,
+                                      //&first, &last,
+                                      //&self->number))
+        //return -1;
+
+    //if (first) {
+        //tmp = self->first;
+        //Py_INCREF(first);
+        //self->first = first;
+        //Py_XDECREF(tmp);
+    //}
+
+    //if (last) {
+        //tmp = self->last;
+        //Py_INCREF(last);
+        //self->last = last;
+        //Py_XDECREF(tmp);
+    //}
+
+    return 0;
+}
+
+/** The python class members for ICPPrinter. */
+static PyMethodDef ICPPrinter_methods[] = {
+    //{"to_string", (PyCFunction)ICPPrinter_to_string, METH_ARGS,
+     //"Prints information about the image and sends it to the gui."
+    //},
+    {NULL}  /* Sentinel */
+};
+
+
+/** The PyTypeObject for ICPPrinter.  This defines how ICPPrinter behaves in
+ * python. */
+static PyTypeObject ICPPrinterType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "icp.ICPPrinter",             /*tp_name*/
+    sizeof( ICPPrinter ),             /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    (destructor)ICPPrinter_dealloc, /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+    "GDB Pretty Printer that creates a Qt/VTK gui displaying images passed to it.",           /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    0,                         /* tp_iter */
+    0,                         /* tp_iternext */
+    ICPPrinter_methods,             /* tp_methods */
+    0,             /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)ICPPrinter_init,      /* tp_init */
+    0,                         /* tp_alloc */
+    ICPPrinter_new,                 /* tp_new */
+};
 
 /** Create the lookup_function Python function.  This is a function associated
  * with the module expected by my version of gdb.pretty used for by register
@@ -32,7 +160,6 @@ PyObject * lookup_function()
   return lookupFunction;
 }
 
-
 extern "C"
 {
 
@@ -56,6 +183,12 @@ PyMODINIT_FUNC  initicp(void)
   icpError = PyErr_NewException(const_cast<char*>("icp.error"), NULL, NULL);
   Py_INCREF(icpError);
   PyModule_AddObject( m, "error", icpError );
+
+  // Create the ICPPrinter gdb pretty printer.
+  if( PyType_Ready( &ICPPrinterType ) < 0 )
+    return;
+  Py_INCREF( &ICPPrinterType );
+  PyModule_AddObject( m, "ICPPrinter", (PyObject *)&ICPPrinterType );
 
   // Create lookup_function.
   PyObject * lookupFunction = lookup_function();
