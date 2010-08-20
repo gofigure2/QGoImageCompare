a1fe3d48d55415061a16c5fe23c39a546fcbe6d4
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-08-20 00:17:46 -0500

    Test the pretty printer.
    It only complements your image at the moment, but it is a start.

--- Testing/Code/GDBPretty/pretty-executable.cpp
+++ Testing/Code/GDBPretty/pretty-executable.cpp
@@ -10,5 +10,24 @@ int main( int argc, char** argv )
     return 1;
     }
 
+  const unsigned int Dimension = 3;
+  typedef float PixelType;
+  typedef itk::Image< PixelType, Dimension > ImageType;
+
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  try
+    {
+    reader->Update();
+    }
+  catch( itk::ExceptionObject& e )
+    {
+    std::cerr << "Error: " << e << std::endl;
+    return 1;
+    }
+
+  ImageType::ConstPointer output = reader->GetOutput();
   return 0;
 }
